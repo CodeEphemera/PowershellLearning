728006871e0bf9557b430c056a427416fb2d0df5
Patrick Felts <patrickfelts@outlook.com>
2025-01-03 16:38:13 -0500

    HW First Commit

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,2 +1,13 @@
-#placeholdercontent
-#SecondCommit
+Write-Host "Hello World" -ForegroundColor Cyan
+
+$TestVar = "Injected Text"
+$TestVar = "Injected Text2"
+$TestVar = "Injected Text3"
+#$TestVar = $null
+Write-Host "Hello World $TestVar"
+
+Write-Host 'Hello World $TestVar'
+#Testing
+Write-Host "Hello World $($TestVar)"
+
+Get-Help Write-Host -Full
